17eda413f9b038b4d29f7efb8c99a8bf5da5efb0
Dario Varotto <dariosky@gmail.com>
2018-03-26 20:22:33 +0200

    Graceful reload command. Gunicorn logs

--- guniconfig.py
+++ guniconfig.py
@@ -1,5 +1,12 @@
+import os
+
+PROJECT_PATH = os.path.dirname(__file__)
+os.makedirs(os.path.join(PROJECT_PATH, 'logs'), exist_ok=True)
+
 bind = "{host}:{port}".format(host='127.0.0.1', port=31045)
 
 workers = 1
-
+# worker_class = 'eventlet'
 proc_name = "darioCV"
+errorlog = os.path.join(PROJECT_PATH, 'logs', 'gerror.log')
+accesslog = os.path.join(PROJECT_PATH, 'logs', 'gaccess.log')
