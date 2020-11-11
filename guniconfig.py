3dc35404bed50516592cb84ca44ec732f3838e0f
Dario Varotto <dariosky@gmail.com>
2020-11-11 23:32:36 +0100

    CUSTOM_PORT env setting

--- guniconfig.py
+++ guniconfig.py
@@ -3,7 +3,9 @@ import os
 PROJECT_PATH = os.path.dirname(__file__)
 os.makedirs(os.path.join(PROJECT_PATH, 'logs'), exist_ok=True)
 
-bind = "{host}:{port}".format(host='127.0.0.1', port=31045)
+port = os.environ.get('CUSTOM_PORT', '11563')
+
+bind = "{host}:{port}".format(host='127.0.0.1', port=int(port))
 
 workers = 1
 # worker_class = 'eventlet'
